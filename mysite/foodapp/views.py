9d1e249bc71adbf00e27453214720a2c2e08e505
Danny Pham <n.danny.pham@gmail.com>
2018-04-01 00:24:52 -0700

    Included results and loading in View

--- mysite/foodapp/views.py
+++ mysite/foodapp/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 def index(request):
 	return render(request, 'foodapp/index.html')
 
+def loading(request):
+	return render(request, 'foodapp/loading.html')
+
+def results(request):
+	return render(request, 'foodapp/results.html')
+
+
