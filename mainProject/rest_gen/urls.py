d6924dfe4fb4c9ae0a746a71a5e03ca561ee6789
patrickchau <patrickchau59@gmail.com>
2018-03-31 22:32:14 -0700

    Added saving forms

--- mainProject/rest_gen/urls.py
+++ mainProject/rest_gen/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:restaurant_id>/', views.detail, name = 'detail')
+    path('<int:restaurant_id>/', views.detail, name = 'detail'),
+    path( 'loading/', views.loading, name = 'loading' )
 ]
