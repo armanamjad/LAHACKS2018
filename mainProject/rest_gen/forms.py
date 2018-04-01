87c3a76ad930018cdb8e9cf498d616ecd9bf3e4f
patrickchau <patrickchau59@gmail.com>
2018-04-01 04:37:11 -0700

    Added cuisine types

--- mainProject/rest_gen/forms.py
+++ mainProject/rest_gen/forms.py
@@ -3,6 +3,12 @@ from django.forms import widgets
 
 class LocationForm (forms.Form):
     location = forms.CharField(label='location', max_length = 100)
-    distance = forms.IntegerField( label = 'distance' )
-    meal = forms.MultipleChoiceField(choices = {('1','Breakfast'),('2','Lunch'),('3','Dinner')}, label = 'meals', widget = widgets.CheckboxSelectMultiple())
+    distance = forms.IntegerField( label = 'distance')
+    meal = forms.MultipleChoiceField(choices = {('Lunch','Lunch'),('Breakfast','Breakfast'),('Dinner','Dinner')}, label = 'meals', widget = widgets.CheckboxSelectMultiple())
+        
+    cuisine = forms.MultipleChoiceField(
+        choices = { ('American','American'),('Asian','Asian'),('Desserts','Desserts'),
+        ('Bubble Tea','Boba'),('Italian','Italian'),('Mexican','Mexican'),('Indian','Indian') }
+        , label = 'cuisine', widget = widgets.CheckboxSelectMultiple())
+
     
